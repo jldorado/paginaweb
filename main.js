2508cfeaaee99b9007822d982c85a8f26a542929
Jose Dorado <joseluisdpl@gmail.com>
2023-07-28 12:27:26 -0400

    Added background randomizer color function

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 
-var miNumero = 2;
-var cantidad = -3;
+var toggleButton = document.getElementById("toggle"); //Conseguimos la referencia del objeto que queremos al html usando el id
 
-function ImprimirMensaje2()
-{
-    if (cantidad <0){
-        console.log( "Cantidad negativa");
-
-    }
+//Declaramos nuestras funciones
+function changeBackgroundColor() {
+    var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
 }
 
-function ImprimirMensaje()
+function PrintMessage()
 {
-    if(miNumero<2)
-    {
-        console.log("Hola estoy es javascript");
-    }else{
-        console.log("Que bueno ya  es javascript");
-    }
+    console.log("Boton apretado");
 }
+////
 
-ImprimirMensaje2(); //Invocando la funcion
-
+//Suscribimos nuestras funciones al evento del boton click
+toggleButton.addEventListener("click",
+()=>{ //Funciones virtuales
+    changeBackgroundColor(); //LLamamos nuestra logica cuando el click sucede
+});

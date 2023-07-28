5da385aa8499afd28ce9b6ff1bcff59fc97c4926
Jose Dorado <joseluisdpl@gmail.com>
2023-07-28 11:47:09 -0400

    Agregamos archivo javascript

---
+++ main.js
@@ -0,0 +1,24 @@
+
+var miNumero = 2;
+var cantidad = -3;
+
+function ImprimirMensaje2()
+{
+    if (cantidad <0){
+        console.log( "Cantidad negativa");
+
+    }
+}
+
+function ImprimirMensaje()
+{
+    if(miNumero<2)
+    {
+        console.log("Hola estoy es javascript");
+    }else{
+        console.log("Que bueno ya  es javascript");
+    }
+}
+
+ImprimirMensaje2(); //Invocando la funcion
+
